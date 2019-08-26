d9d9eac0674fc6a2d9acc48b561e84fc45dd3505
Andrei Brabete <andreibrabete@gmail.com>
2019-08-26 16:29:59 +0100

    optimised imports

--- src/test/java/com/example/continuousdelivery/domain/UserTest.java
+++ src/test/java/com/example/continuousdelivery/domain/UserTest.java
@@ -3,8 +3,6 @@ package com.example.continuousdelivery.domain;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
